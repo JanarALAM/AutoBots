df803ebd8690b7907433003b72718272da19177d
Janara <mairambekovajanar15@gmail.com>
2025-07-08 18:45:57 +0300

    Version4.0.1

--- src/main/java/com/autobots/NewVersionClass.java
+++ src/main/java/com/autobots/NewVersionClass.java
@@ -2,6 +2,6 @@ package com.autobots;
 
 public class NewVersionClass {
     public static void main(String[] args) {
-        System.out.println("Now vesion 4.0.0");
+        System.out.println("Now version 4.0.0");
     }
 }
